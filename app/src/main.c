e32d96ea37694ce80c9f988dea3a6a438e164471
Krystof Hes <krystof.hes@seznam.cz>
2024-10-12 16:43:19 +0200

    commands for turning LED on and OFF

--- app/src/main.c
+++ app/src/main.c
@@ -9,7 +9,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-
 // Zephyr-specific libs
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
@@ -25,7 +24,7 @@
 #include <zephyr/version.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   500
+#define SLEEP_TIME_MS 500
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
@@ -34,7 +33,7 @@
  * Ensure that an overlay for USB serial has been defined.
  */
 BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
-	    "Console device is not ACM CDC UART device");
+			 "Console device is not ACM CDC UART device");
 
 LOG_MODULE_REGISTER(main, CONFIG_MAIN_LOG_LEVEL); // registers the log level for the module MAIN specified in Kconfig
 
@@ -45,6 +44,7 @@ LOG_MODULE_REGISTER(main, CONFIG_MAIN_LOG_LEVEL); // registers the log level for
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 // Example shell command handler
+// Prints "pong" as a response to "ping" request
 static int cmd_demo_ping(const struct shell *sh, size_t argc, char **argv)
 {
 	ARG_UNUSED(argc);
@@ -55,21 +55,35 @@ static int cmd_demo_ping(const struct shell *sh, size_t argc, char **argv)
 	return 0;
 }
 
+static int cmd_led_on(const struct shell *sh, size_t argc, char **argv)
+{
+	// Turn ON LED
+	return (gpio_pin_set_dt(&led, 1) < 0) ? 1 : (shell_print(sh, "LED on"), 0);
+}
+
+static int cmd_led_off(const struct shell *sh, size_t argc, char **argv)
+{
+	// Turn OFF LED
+	return (gpio_pin_set_dt(&led, 0) < 0) ? 1 : (shell_print(sh, "LED OFF"), 0);
+}
+
 // Register commands
 SHELL_CMD_REGISTER(ping, NULL, "Respond with pong", cmd_demo_ping);
+SHELL_CMD_REGISTER(led_on, NULL, "Turn the LED ON", cmd_led_on);
+SHELL_CMD_REGISTER(led_off, NULL, "Turn the LED OFF", cmd_led_off);
 
 int main(void)
 {
-    LOG_INF("Program starting"); // example info message
+	LOG_INF("Program starting"); // example info message
 	int ret;
-	bool led_state = true;
-
-	if (!gpio_is_ready_dt(&led)) {
+	if (!gpio_is_ready_dt(&led))
+	{
 		return 0;
 	}
 	// Turn ON LED
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
+	if (ret < 0)
+	{
 		return 0;
 	}
 	// Shell config
@@ -77,24 +91,28 @@ int main(void)
 	uint32_t dtr = 0;
 
 	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_shell_uart));
-	if (!device_is_ready(dev) || usb_enable(NULL)) {
+	if (!device_is_ready(dev) || usb_enable(NULL))
+	{
 		return 0;
 	}
 
-	while (!dtr) {
+	while (!dtr)
+	{
 		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
 		k_sleep(K_MSEC(100));
 	}
 
 	LOG_INF("USB device configured and connected");
-    // this loop is needed to define how often log messages are processed
-    while (true) {
-        k_msleep(30);   /* sleep 30 ms*/
-    }
+	// this loop is needed to define how often log messages are processed
+	while (true)
+	{
+		k_msleep(30); /* sleep 30 ms*/
+	}
 
 	// Turn OFF LED at the end
 	ret = gpio_pin_set_dt(&led, 0);
-	if (ret < 0) {
+	if (ret < 0)
+	{
 		return 0;
 	}
 
