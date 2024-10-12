962317355144bc6032a20bc3ac98168f1eca7a7c
Krystof Hes <krystof.hes@seznam.cz>
2024-10-12 11:36:22 +0200

    USB console

--- app/src/main.c
+++ app/src/main.c
@@ -5,14 +5,29 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+// Zephyr-specific libs
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/drivers/uart.h>
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   500
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+
+/*
+ * Ensure that an overlay for USB serial has been defined.
+ */
+BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
+	    "Console device is not ACM CDC UART device");
+
+LOG_MODULE_REGISTER(main, CONFIG_MAIN_LOG_LEVEL); // registers the log level for the module MAIN specified in Kconfig
 
 /*
  * A build error on this line means your board is unsupported.
@@ -22,6 +37,22 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
+	// Configure USB Serial for Console output
+	const struct device *usb_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+	uint32_t dtr = 0;
+
+	if (usb_enable(NULL)) {
+		return 1;
+	}
+
+	// Sleep to wait for a terminal connection. To wait until connected, comment out
+	// these two lines and uncomment the while below.
+	k_sleep(K_MSEC(2500));
+	uart_line_ctrl_get(usb_dev, UART_LINE_CTRL_DTR, &dtr);
+
+
+
+    LOG_INF("Program starting"); // example info message
 	int ret;
 	bool led_state = true;
 
@@ -41,7 +72,7 @@ int main(void)
 		}
 
 		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
+		printk("LED state: %s\n", led_state ? "ON" : "OFF");
 		k_msleep(SLEEP_TIME_MS);
 	}
 	return 0;
