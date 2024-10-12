d0c920f9e43cc9b98151d38148b631ba4b071cdb
Krystof Hes <krystof.hes@seznam.cz>
2024-10-12 19:11:38 +0200

    led blink using work

--- app/src/main.c
+++ app/src/main.c
@@ -41,7 +41,7 @@ LOG_MODULE_REGISTER(main, CONFIG_MAIN_LOG_LEVEL); // registers the log level for
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios); // LED device
 
 // Example shell command handler
 // Prints "pong" as a response to "ping" request
@@ -51,6 +51,18 @@ static int cmd_demo_ping(const struct shell *sh, size_t argc, char **argv)
 	ARG_UNUSED(argv);
 
 	shell_print(sh, "pong");
+
+	return 0;
+}
+
+// Print out command with provided arguments
+static int cmd_demo_params(const struct shell *sh, size_t argc, char **argv)
+{
+	shell_print(sh, "argc = %zd", argc);
+	for (size_t cnt = 0; cnt < argc; cnt++)
+	{
+		shell_print(sh, "  argv[%zd] = %s", cnt, argv[cnt]);
+	}
 
 	return 0;
 }
@@ -67,20 +79,72 @@ static int cmd_led_off(const struct shell *sh, size_t argc, char **argv)
 	return (gpio_pin_set_dt(&led, 0) < 0) ? 1 : (shell_print(sh, "LED OFF"), 0);
 }
 
+// Init work items
+static int blink_period = 500; // in ms
+static struct k_work_delayable led_work;
+
+static void led_blink_work(struct k_work *work)
+{
+	// Toggle LED
+	int ret;
+	ret = gpio_pin_toggle_dt(&led);
+	// Reschedule the work to be done again in half of the period ms
+	k_work_reschedule(&led_work, K_MSEC(blink_period / 2)); // provide time as k_ticks_t using the macro for conversion
+}
+
+// Stop the blink work
+static void stop_blinking()
+{
+	int ret = k_work_cancel_delayable(&led_work);
+	if (ret == 0)
+	{
+		printk("Blinking stopped successfully.\n");
+	}
+	else if (ret == -EALREADY)
+	{
+		printk("Blinking was already stopped.\n");
+	}
+	else
+	{
+		printk("Failed to stop blinking.\n");
+	}
+}
+
+// Blink the led with a set period in ms
+static int cmd_led_blink(const struct shell *sh, size_t argc, char **argv)
+{
+	blink_period = atoi(argv[1]); // store the argument in global variable
+	if (blink_period > 0)
+	{
+		stop_blinking(); // stop previous work processing to react to new command exec
+		shell_print(sh, "Starting to blink with period %d ms", blink_period);
+		k_work_init_delayable(&led_work, led_blink_work);
+		k_work_schedule(&led_work, K_NO_WAIT); // schedule the first execution to start immediately
+	}
+	else
+	{
+		shell_print(sh, "Setting blink period not successful");
+		return 1;
+	};
+	return 0;
+}
+
 // Register commands
 SHELL_CMD_REGISTER(ping, NULL, "Respond with pong", cmd_demo_ping);
+SHELL_CMD_REGISTER(params, NULL, "Print params command.", cmd_demo_params);
 SHELL_CMD_REGISTER(led_on, NULL, "Turn the LED ON", cmd_led_on);
 SHELL_CMD_REGISTER(led_off, NULL, "Turn the LED OFF", cmd_led_off);
+SHELL_CMD_ARG_REGISTER(led_blink, NULL, "Blink the LED with a defined period in ms.", cmd_led_blink, 2, NULL); // 2 mandatory args - the command name and ther period
 
 int main(void)
 {
-	LOG_INF("Program starting"); // example info message
+	LOG_INF("Program starting"); // example info messag
+	// Init the LED device in logic 1 state
 	int ret;
 	if (!gpio_is_ready_dt(&led))
 	{
 		return 0;
 	}
-	// Turn ON LED
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0)
 	{
@@ -89,31 +153,21 @@ int main(void)
 	// Shell config
 	const struct device *dev;
 	uint32_t dtr = 0;
-
 	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_shell_uart));
 	if (!device_is_ready(dev) || usb_enable(NULL))
 	{
 		return 0;
 	}
-
-	while (!dtr)
+	while (!dtr) // wait for shell device to be ready
 	{
 		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
 		k_sleep(K_MSEC(100));
 	}
-
 	LOG_INF("USB device configured and connected");
-	// this loop is needed to define how often log messages are processed
+	// Wait in this loop for shell commands or process messages
 	while (true)
 	{
 		k_msleep(30); /* sleep 30 ms*/
-	}
-
-	// Turn OFF LED at the end
-	ret = gpio_pin_set_dt(&led, 0);
-	if (ret < 0)
-	{
-		return 0;
 	}
 
 	return 0;
